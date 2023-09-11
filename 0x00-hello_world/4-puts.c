aa932996e5aedcc961245b5593eabc9d26e8cfdd
kevsamdev <kevinsamson45@gmail.com>
2023-09-11 13:19:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h/**
+ * main - a PROGRAM THAT PRINTS A LINE WITH PUTS FUNCTION
+ * Return: 0(Success)
+ */int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
+}
