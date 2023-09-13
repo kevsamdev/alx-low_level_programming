ef4150072f663d44dcb0e30e373677a345a18222
kevsamdev <kevinsamson45@gmail.com>
2023-09-13 13:12:23 -0700

    0x02-functions_nested_loops

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that prints size of various types on the computer.
+ * Return 0 (Success)
+*/
+int main(void)
+{
+	char a;
+	int  b;
+	long int  c;
+	long long int d;
+	float f;
+printf("Size of a char: %lu bytes(s)\n", (unsigned long)
+sizeofof(a));
+printf("Size of a char: %lu bytes(s)\n", (unsigned long)
+sizeofof(b));
+printf("Size of a char: %lu bytes(s)\n", (unsigned long)
+sizeofof(c));
+printf("Size of a char: %lu bytes(s)\n", (unsigned long)
+sizeofof(d));
+printf("Size of a char: %lu bytes(s)\n", (unsigned long)
+sizeofof(f));
+return (0);
