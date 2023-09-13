dfa474df6556b2c5720ed3cf3ca751b810e9a4e5
kevsamdev <kevinsamson45@gmail.com>
2023-09-13 15:05:30 -0700

    putcha2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,11 @@
  */
 int main(void)
 {
-    int i;
-    char c[] = "_putchar";
-
-    for (i = 0; i < 8; i++)
-   {putchar(c[i]);
-    }
-
-    putchar('\n');
-    return (0);
+int i;
+char c[] = "_putchar";
+for (i = 0; i < 8; i++)
+{putchar(c[i]);
+}
+putchar('\n');
+return (0);
 }
