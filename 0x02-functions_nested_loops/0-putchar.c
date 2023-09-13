871fc9a5862c0dcb1a7b752f9a30ec04d30d6962
kevsamdev <kevinsamson45@gmail.com>
2023-09-13 14:12:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print_putchar
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i])
+	}
+	putchar('\n');
+	return (0);
+}
