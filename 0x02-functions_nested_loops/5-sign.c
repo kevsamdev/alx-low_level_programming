4f1216c4166d4c0f5a4b104979f224c14269f0f3
kevsamdev <kevinsamson45@gmail.com>
2023-09-13 17:27:52 -0700

    Functions, nested loops2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -24,5 +23,5 @@ else if (n < 0)
 _putchar('-');
 return (-1);
 }
-return (0;
+return (0);
 }
