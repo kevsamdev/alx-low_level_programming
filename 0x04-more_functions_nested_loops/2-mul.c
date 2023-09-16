a358ed639593a06d9972b63f0cc52dd60202f7fe
kevsamdev <kevinsamson45@gmail.com>
2023-09-15 17:57:20 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul is a function that checks for a digit
+* @a: a - variable
+* @b: b - variable
+* Return: addition of the multiplicattion
+*/
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
