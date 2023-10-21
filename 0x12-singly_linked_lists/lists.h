3937af600c1e98aac0e8a4f4ceb9c0df55f5c5e9
kevsamdev <kevinsamson45@gmail.com>
2023-10-21 05:44:49 -0700

    revision01

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * project by Kevin
  */
 typedef struct list_s
 {
@@ -12,3 +13,11 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
