a700a7a3e9aef89bf81aa2033c58d32702a923c7
kevsamdev <kevinsamson45@gmail.com>
2023-12-15 10:48:24 -0800

    0x18 100 operations

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - add two integars
+ * @a: first int
+ * @b: the second int
+ * Return: sum
+*/
+int add (int a, int b)
+{
+    return (a + b);
+}
+/**
+ * sub - subtract two integars
+ * @a: first int
+ * @b: secomd int
+ * Return: subtracted final result
+*/
+int sub (int a, int b)
+{
+    return (a - b);
+}
+/**
+ * div - divide two integars
+ * @a: first int
+ * @b: second int
+ * Return: division result
+*/
+int div(int a, int b)
+{
+    return (a / b);
+}
+/**
+ * mod - mod of two integars
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+*/
+int mod (int a, int b)
+{
+    return (a % b);
+}
+/**
+ * mul - multiply two integars
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+*/
+int mul (int a, int b)
+{
+    return (a * b);
+}
